7138e762a4341498df9922ff3458ca0a67593506
Shane <shanepwasylow@gmail.com>
2026-03-04 11:00:27 +0100

    Adding SQL script to repository
    SQL scripts exluding prohibited items

---
+++ exclude_queries.sql
@@ -0,0 +1,8 @@
+--Excluding items based on their category 
+SELECT * 
+FROM items WHERE country IN ('DE', 'UK') 
+AND category NOT IN ( 'medications_prescription', 'dangerous_items' );
+
+--Excluding items based on their name 
+SELECT * FROM items WHERE country IN ('DE', 'UK')
+AND name NOT LIKE '%ninja%' AND name NOT LIKE '%daggers%';
